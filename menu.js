7d1d8d3fb9ff26043a69ef74811da73591c55cbf
Jefferson Santos <jeff.lssantos@outlook.com>
2016-02-24 16:20:46 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,11 +1,13 @@
-$("#menu-open").click(function() {
-    $(".menu-header").css("top", 0);
-    $(this).css("display", "none");
-    $("#menu-close").css("display", "block");
-});
-
-$("#menu-close").click(function() {
-    $(".menu-header").css("top", -59);
-    $(this).css("display", "none");
-    $("#menu-open").css("display", "block");
+$(document).ready(function(){
+    $("#menu-open").click(function() {
+        $(".menu-header").css("top", 0);
+        $(this).css("display", "none");
+        $("#menu-close").css("display", "block");
+    });
+    
+    $("#menu-close").click(function() {
+        $(".menu-header").css("top", -59);
+        $(this).css("display", "none");
+        $("#menu-open").css("display", "block");
+    });
 });
