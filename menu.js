0b3e471c35f7a8b7a979c07f8c9b893177e59b20
Jefferson Santos <jefflssantos@gmail.com>
2016-02-26 17:26:58 -0300

    alert testing

--- menu.js
+++ menu.js
@@ -4,10 +4,12 @@ window.onload = function() {
         $(this).css("display", "none");
         $("#menu-close").css("display", "block");
     });
-    
+
     $("#menu-close").click(function() {
         $(".menu-header").css("top", -59);
         $(this).css("display", "none");
         $("#menu-open").css("display", "block");
     });
+
+    alert('asd');
 };
